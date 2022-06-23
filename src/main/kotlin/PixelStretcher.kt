deffcc13f01c8d7d289ecd46686912d3b8269b9d
James Baker <j_d_baker@hotmail.com>
2022-06-23 16:06:28 +0100

    Add fade

--- src/main/kotlin/PixelStretcher.kt
+++ src/main/kotlin/PixelStretcher.kt
@@ -2,9 +2,14 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.context
 import com.github.ajalt.clikt.output.CliktHelpFormatter
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.choice
 import com.github.ajalt.clikt.parameters.types.file
 import com.sksamuel.scrimage.ImmutableImage
 import com.sksamuel.scrimage.nio.PngWriter
+import java.awt.Color
+import java.awt.Transparency
 import java.io.File
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -21,31 +26,62 @@ object PixelStretcher : CliktCommand(
 
     private val inputPath by argument().file(mustExist = true, canBeDir = false)
 
+    private val type by option().choice("down", "fade").default("down")
+
     override fun run() {
 
         val input = inputPath.immutableImage()
 
-        val xyToColor = (0 until input.width)
-            .map { x ->
-                (sequenceOf(input.height) + (0 until input.height).reversed()
-                    .asSequence()
-                    .filter {y -> input.color(x,y).alpha == 255  })
-                    .zipWithNext { y1,y2 -> if (y1 - 1 != y2) {
-                        ((y1 - 1) downTo (y2 + 1)) to input.color(x,y2)
-                    } else {null}}
-                    .filterNotNull()
-                    .toList()
-            }
+        when (type) {
+            "down" -> {
+                val lastColor = arrayOfNulls<Color>(input.width)
 
-        val totalPixels = input.pixels().size
-        var count = 0
-        input.map { pixel ->
-            println("${++count} of $totalPixels")
-            when {
-                pixel.alpha() == 255 -> pixel.toColor().awt()
-                else -> xyToColor[pixel.x].firstOrNull { pixel.y in it.first }?.second?.awt() ?: pixel.toColor().awt()
+                val totalPixels = input.pixels().size.toDouble()
+                var count = 0
+                input.map { pixel ->
+                    println(++count / totalPixels)
+                    when {
+                        pixel.alpha() == 255 -> pixel.toColor().awt().also { lastColor[pixel.x] = it }
+                        else -> lastColor[pixel.x] ?: pixel.toColor().awt()
+                    }
+                }.save()
             }
-        }.save()
+            "fade" -> {
+                val fade = Array<Array<Color?>>(input.width) { arrayOfNulls(input.height) }
+
+                (0 until input.width).forEach { x->
+                    input.col(x).asSequence().withIndex()
+                        .filter { it.value.toColor().awt().transparency == Transparency.OPAQUE }
+                        .zipWithNext { (indexA, pixelA), (indexB, pixelB) ->
+                            if (indexA + 1 != indexB) {
+                                val range = (indexA + 1) until indexB
+                                range.zip(
+                                        fadeColor(
+                                            start = pixelA.toColor().awt(),
+                                            end = pixelB.toColor().awt(),
+                                            steps = range.count(),
+                                        )
+                                    )
+                            } else {
+                                emptyList()
+                            }
+                        }
+                        .flatten()
+                        .forEach { (y, color) ->  fade[x][y] = color }
+                }
+
+                val totalPixels = input.pixels().size.toDouble()
+                var count = 0
+                input.map { pixel ->
+                    println(++count / totalPixels)
+                    when {
+                        pixel.alpha() == 255 -> pixel.toColor().awt()
+                        else ->  fade[pixel.x][pixel.y] ?: pixel.toColor().awt()
+                    }
+                }.save()
+            }
+            else -> error("type = $type")
+        }
     }
 
     private fun ImmutableImage.save(): File? {
@@ -59,3 +95,16 @@ fun main(argv: Array<String>) = PixelStretcher.main(argv)
 
 private fun File.immutableImage() = ImmutableImage.loader().fromFile(this)
 
+private fun fadeColor(start: Color, end: Color, steps: Int): List<Color> {
+    require(steps > 0)
+    return (0 until steps)
+        .map { it.toDouble() + 0.5 }
+        .map { it / steps }
+        .map { fraction ->
+            Color(
+               (fraction * end.red + (1 - fraction) * start.red).toInt(),
+               (fraction * end.green + (1 - fraction) * start.green).toInt(),
+               (fraction * end.blue + (1 - fraction) * start.blue).toInt(),
+            )
+        }
+}
